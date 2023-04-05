48970587a1182a7dc9f62f868ce9f9796fadd663
bondanjody <bondanjodys@gmail.com>
2023-04-06 01:44:19 +0700

    Menambahkan koneksi ke MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,28 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://bondanjs:bondanjs@bondanjody.5piu27x.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    // Menutup koneksi ke database
+    client.close();
+
+    // Memberi response
+    res.status(201).json({ message: "Meetups inserted!" });
   }
 }
 
